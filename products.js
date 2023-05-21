b07b3d70c58ee9f6d202611c90a0ad323156790c
Vraj-1999 <109271086+Vraj-1999@users.noreply.github.com>
2023-05-20 18:14:59 -0700

    final

---
+++ products.js
@@ -0,0 +1,215 @@
+
+
+window.products = [
+    {
+        id: "1",
+        discontinued: true,
+        title: "Gildan Mens Men's Fleece Hooded Sweatshirt",
+        price: 1690,
+        description: "Gildan environmental program accomplishes two core objectives: reduce our environmental impact and preserve the natural Resources being used in our manufacturing process.",
+        Review:"⭐⭐⭐⭐⭐",
+        category: ["1"],
+        imageUrl: "https://m.media-amazon.com/images/I/61qTPaU7dYL._AC_UX679_.jpg",
+        
+    },
+    {
+        id: "2",
+        discontinued: true,
+        title: "Hanes Mens EcoSmart Fleece Sweatshirt",
+        price: 1153,
+        imageUrl: "https://i5.walmartimages.com/asr/64cb93cd-b45e-4d20-96b8-8234b8c754d9_1.09cdcdf2e575286b9193df286aeeac20.jpeg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "Why fuss with a zipper just pull this classic, medium-weight 7.8-O fleece sweatshirt over your head and get going.",
+        category: ["1"]
+    },
+    {
+        discontinued: true,
+        id: "3",
+        title: "Jerzees Mens Black Adult Pullover Hooded Sweatshirt",
+        price: 1799,
+        imageUrl: "https://m.media-amazon.com/images/I/81wR0ERQHGL._AC_UY550_.jpg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "50% Cotton, 50% Polyester Pull On closure Machine Wash 50 percent cotton, 50 percent polyester Soft,",
+        category: ["1"]
+    },
+    {
+        discontinued: true,
+        id: "4",
+        title: "Carhartt Mens B&t Midweight Original Fit Hooded Pullover Sweatshirt K121",
+        price: 5499,
+        imageUrl: "https://litb-cgis.rightinthebox.com/images/x/202211/bps/product/inc/uwzwlm1668506996223.jpg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "Sometimes mother nature can't make up her mind. This sweatshirt works perfectly as a layer or by itself ", 
+        category: ["1"]
+    },
+    {
+        discontinued: true,
+        id: "5",
+        title: "BASSDASH Mens Fishing Fleece Hoodie with Neck Gaiter Water Resistant Performance Hunting Sweatshirt FS18M",
+        price: 69500,
+        imageUrl: "https://litb-cgis.rightinthebox.com/images/x/202211/bps/product/inc/uwzwlm1668506996223.jpg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "Water and stain resistant treatment makes dirt easier to clean as well as making the sweatshirt a nice gear for fishing on cool rainy days; ",
+        category: ["1"]
+    },
+    {
+        discontinued: true,
+        id: "6",
+        title: "Zilcremo",
+        price: 2999,
+        imageUrl: "https://m.media-amazon.com/images/I/51DLpG9YMaL._AC_UY879_.jpg",
+        Review:"⭐⭐⭐",
+        description: "Zilcremo Women 2023 Summer Sleeveless Dress Casual V Neck Loose Split Tshirt Long Dresses",
+        category: ["2"]
+    },
+    {
+        discontinued: true,
+        id: "7",
+        title: "OUGES Womens Summer Dress Short Sleeve V Neck Casual Floral Wrap Dress with Pockets",
+        price: 999,
+        imageUrl: "https://m.media-amazon.com/images/I/71tfeyIR4LL._AC_UX679_.jpg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "Short sleeve, v-neck, A-line, floral patterned with both side pockets design, breathable, skin-touch,",
+        category: ["2"], 
+    },
+    {
+        discontinued: true,
+        id: "8",
+        title: "Sopliagon Women Summer Tie Back Latern Sleeve Square Neck Ruffle Dress Elastic Waist Swing Casual Mini Dresses",
+        price: 1099,
+        imageUrl: "https://m.media-amazon.com/images/I/51X6dTbSgjL._AC_UX569_.jpg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "Features: Womens summer chiffon novelty dress,featuring with 1/2 lantern sleeve and square neck.Chiffon lining is added inside to prevent transparency.",
+        category: ["2"]
+    },
+    {
+        discontinued: true,
+        id: "9",
+        title: "YACUN Women Batwing Sleeve Bodycon Work Dresses High Waist Business Casual Office Pencil Dress with Belt",
+        price: 6400,
+        imageUrl: "https://m.media-amazon.com/images/I/51fQ9u03kIL._AC_UY879_.jpg",
+        Review:"⭐⭐⭐",
+        description: "Perfect Leggings for yoga, exercise, fitness, gym, running, any type of workout or everyday use. BISON active wear combine fashion, function and performance",
+        category: ["2"]
+    },
+    {
+        discontinued: true,
+        id: "10",
+        title: "High Rise Vintage Slim Jeans with Washwell",
+        price: 10900,
+        imageUrl: "https://www.gapcanada.ca/webcontent/0051/605/256/cn51605256.jpg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "Perfect for any season, setting, or occasion - The Satina stretch high waisted capri leggings are your “go to” everyday leggings. ",
+        category: ["2"]
+    },
+    {
+        discontinued: true,
+        id: "11",
+        title: "Mizuno T10 Plus Volleyball Knee Pads",
+        price: 10900,
+        imageUrl: "https://m.media-amazon.com/images/I/915-pJ7SxpL._AC_SX679_.jpg",
+        Review: '⭐⭐⭐⭐⭐',
+        description: "Mizuno T10 plus now features a slimmer design. Embroider Mizuno run bird logo. Enhanced foam to protect you where you need it. One Size fits most. ",
+        category: ["3"]
+    },
+    {
+        discontinued: true,
+        id: "12",
+        title: "Hanes mens Hanes Men's Max Cushion Crew Socks",
+        price: 11400,
+        imageUrl: "https://m.media-amazon.com/images/I/81vTEPIns+L._AC_UX679_.jpg",
+        Review: '⭐⭐⭐⭐',
+        description: "Hanes Max Cushion 6-Pack Crew socks pack a powerful punch with double tough durability and maximum cushioning around the foot for maximum comfort. ",
+        category: ["3"]
+    },
+    {
+        discontinued: true,
+        id: "13",
+        title: "Free Bird 99 Unisex Cotton Adjustable Baseball Cap Plain HatAcer SB220Q bi 21.5 inches Full HD (1920 x 1080) IPS Ultra-Thin",
+        price: 59900,
+        imageUrl: "https://media.gucci.com/style/Transparent_Center_0_0_250x170/1519416008/200035_KQWBG_1060_001_100_0000_Light.png",
+        Review:"⭐⭐⭐",
+        description: "100% cotton Button closure Classic baseball cap with various colors available . 6 panel construction with eyelets . ",
+        category: ["3"]
+    },
+    {
+        discontinued: true,
+        id: "14",
+        title: "Spalding Highlight Black/Gold Rubber Outdoor Basketball",
+        price: 99999,
+        imageUrl: "https://m.media-amazon.com/images/I/91lP5fN5yuL._AC_SX679_.jpg",
+        Review:"⭐⭐⭐⭐",
+        description: "Made for pick-up games, driveway ball, and Sundays at the park, this outdoor basketball has a rubber construction that can stand up to asphalt.",
+        category: ["3"]
+    },
+    {
+        discontinued: true,
+        id: "15",
+        title: "PUMA Men s 6 Pack Low Cut Socks",
+        price: 5699,
+        imageUrl: "https://m.media-amazon.com/images/I/61Q4ZsI4fkL._AC_UX569_.jpg",
+        Review:"⭐⭐⭐⭐",
+        description: "98% Polyester, 2% Spandex Machine Wash Smooth toe seam Sport cushion and athletic performance Arch support TT Blend and Half terry",
+        category: ["3"]
+    },
+    {
+        discontinued: true,
+        id: "16",
+        title: "Wyze Cam v3 with Color Night Vision, Wired 1080p HD Indoor/Outdoor Video Camera, ",
+        price: 2995,
+        imageUrl: "https://m.media-amazon.com/images/I/619qhrY0ppL._AC_SX425_.jpg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "The Starlight Sensor can see full color in environments up to 25x darker than traditional video cameras and the new f/1.6 aperture captures 2x lighter. ",
+        category: ["4"]
+    },
+    {
+        discontinued: true,
+        id: "17",
+        title: "Projector Compatible with TV Stick,HDMI,USB,Laptop,iOS & Android",
+        price: 3999,
+        imageUrl: "https://multimedia.bbycastatic.ca/multimedia/products/500x500/158/15829/15829798.jpeg",
+        Review:"⭐⭐⭐",
+        description: "We provide 12 months exchange or money-return guarantee and 3-year repair, lifetime professional technical support. ",
+        category: ["4"]
+    },
+    {
+        discontinued: true,
+        id: "18",
+        title: "Samsung Galaxy Tab A8 (2022) Grey 32GB Android Tablet - 10.5 Display, 8MP+5MP Camera, Long Lasting Battery",
+        price: 985,
+        imageUrl: "https://multimedia.bbycastatic.ca/multimedia/products/500x500/159/15917/15917163.jpg",
+        Review:"⭐⭐",
+        description: "Performance - Get the power, storage and speed your family needs with an upgraded chipset and plenty of room to keep files. ",
+        category: ["4"]
+    },
+    {
+        discontinued: true,
+        id: "19",
+        title: "Echo Dot (3rd gen) - Smart speaker with Alexa – Charcoal",
+        price: 795,
+        imageUrl: "https://multimedia.bbycastatic.ca/multimedia/products/500x500/130/13023/13023977.jpg",
+        Review:"⭐⭐⭐",
+        description: "Voice control your music - Stream songs from Amazon Music, Apple Music, Sirius XM, Spotify, Deezer, and others.",
+        category: ["4"]
+    },
+    {
+        discontinued: true,
+        id: "20",
+        title: "Fire TV Stick 4K streaming device with Alexa Voice Remote",
+        price: 1299,
+        imageUrl: "https://multimedia.bbycastatic.ca/multimedia/products/500x500/153/15386/15386728.jpg",
+        Review:"⭐⭐⭐⭐⭐",
+        description: "Control your smart home - Ask Alexa to check weather, dim the lights, view live camera feeds, stream music and more. Simple and intuitive",
+        category: ["4"]
+    },
+    {
+        discontinued: true,
+        id: "21",
+        title: "Apple AirPods Pro",
+        price: 1299,
+        imageUrl: "https://images.unsplash.com/photo-1606841837239-c5a1a4a07af7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
+        Review:"⭐⭐",
+        description: "Transparency mode for hearing and interacting with the world around you. Spatial audio with dynamic head tracking places sound all around you. ",
+        category: ["4"]
+    }
+];
